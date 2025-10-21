77d517a4512460e79aa0f40f6008696412bb0a60
Nick Housley <nickhousley@Nicks-MacBook-Pro-3.local>
2025-10-21 11:49:50 -0400

    fig_2g and fig_2h

--- code/fig_2_code.R
+++ code/fig_2_code.R
@@ -215,115 +215,36 @@ ggsave(siRNA_endosome_Fig, file = "siRNA_endosome_Fig_2g.pdf", width = 5, height
 
 
 
-
-
-
-
-
-
-
-########################### Figure 5b ###########################
-########################### description
-## TEM modeling
+########################### Figure 5 siRNA release ###########################
 
 ########################### load dependencies
 ########################### Custom Functions
-## Gives count, mean, standard deviation, standard error of the mean, and confidence interval (default 95%).
-##   data: a data frame.
-##   measurevar: the name of a column that contains the variable to be summariezed
-##   groupvars: a vector containing names of columns that contain grouping variables
-##   na.rm: a boolean that indicates whether to ignore NA's
-##   conf.interval: the percent range of the confidence interval (default is 95%)
-summarySE <- function(data=NULL, measurevar, groupvars=NULL, na.rm=FALSE,
-                      conf.interval=.95, .drop=TRUE) {
-  library(plyr)
-  
-  # New version of length which can handle NA's: if na.rm==T, don't count them
-  length2 <- function (x, na.rm=FALSE) {
-    if (na.rm) sum(!is.na(x))
-    else       length(x)
-  }
-  
-  # This does the summary. For each group's data frame, return a vector with
-  # N, mean, and sd
-  datac <- ddply(data, groupvars, .drop=.drop,
-                 .fun = function(xx, col) {
-                   c(N    = length2(xx[[col]], na.rm=na.rm),
-                     mean = mean   (xx[[col]], na.rm=na.rm),
-                     sd   = sd     (xx[[col]], na.rm=na.rm)
-                   )
-                 },
-                 measurevar
-  )
-  
-  # Rename the "mean" column    
-  datac <- rename(datac, c("mean" = measurevar))
-  
-  datac$se <- datac$sd / sqrt(datac$N)  # Calculate standard error of the mean
-  
-  # Confidence interval multiplier for standard error
-  # Calculate t-statistic for confidence interval: 
-  # e.g., if conf.interval is .95, use .975 (above/below), and use df=N-1
-  ciMult <- qt(conf.interval/2 + .5, datac$N-1)
-  datac$ci <- datac$se * ciMult
-  
-  return(datac)
-}
 ########################### Load Data
-tem_allData <- read_excel("data/figure_6/tem_ng_data_raw.xlsx", 
-                          na = "NA")
+heyCellsiRNAsangsCOLOC_allData <- read_excel("data/fig_2/heyCellsiRNAsangsCOLOC.xlsx", 
+                                             na = "NA")
 
 ########################### Data Wrangling
-tem_allData$concentrationFactor <- factor(tem_allData$concentrationFactor, levels=c('ng_0.46', 'ng_0.93', 'ng_1.875', 'ng_3.75', 'ng_7.5', 'ng_30'))
-
-# tem_allDataScaled<-tem_allData %>% mutate_at(c("area"), ~(scale(.) %>% as.vector))
-temSummary <- summarySE(tem_allData, measurevar="area", groupvars=c("concentrationFactor", "concentrationNum"))
-
-
-pd <- position_dodge(0.1) # move them .05 to the left and right
+heyCellsiRNAsangsCOLOC_allData$cell <- factor(heyCellsiRNAsangsCOLOC_allData$cell)
+heyCellsiRNAsangsCOLOC_allData$metric <- factor(heyCellsiRNAsangsCOLOC_allData$metric)
+heyCellsiRNAsangsCOLOC_allData$stage <- factor(heyCellsiRNAsangsCOLOC_allData$stage)
 
 ########################### quick visualization
-
-fig_6g<-temSummary %>%
-  filter(concentrationNum != 'NA') %>%
-  ggplot( aes(x=concentrationNum, y=area, group=1)) + 
-  geom_errorbar(aes(ymin=area-se, ymax=area+se), width=.1, position=pd) +
-  geom_line(position=pd) +
-  geom_point(position=pd)+
-  geom_line(data = temSummary, aes(x = concentrationNum, y = (area)**0+3136, group = 1), 
-            inherit.aes = FALSE,linetype = "dashed")+
-  geom_line(data = temSummary, aes(x = concentrationNum, y = c(66.3044, 134.0502,272.4246, 540.525, 1081.05, 4324.2)*100, group = 1),
-            inherit.aes = FALSE, linetype = "dashed", color="red")+
-  geom_line(data = temSummary, aes(x = concentrationNum, y = (N)*100, group = 1), 
-            inherit.aes = FALSE,linetype = "solid", color="red")+
-  theme_classic()+
-  scale_y_continuous("NG particle area", trans='log2',   sec.axis = sec_axis(~.x * .01, name = "NG particle #"))+
-  scale_x_continuous(trans='log2')+
-  theme(axis.text.y.right = element_text(color = "red"),
-        axis.title.y.right = element_text(color = "red"))
-
-
-
-
-########################### analyses/modeling
-setwd("figures/")
-cairo_ps("fig_6g.eps")
-fig_6g
-invisible(suppressMessages(suppressWarnings(dev.off())))
-
-########################### saving data
+siRNA_sang_Fig<- heyCellsiRNAsangsCOLOC_allData %>% 
+  # filter(metric != "pearsonCorr") %>%
+  filter(metric == "ColocCoeff1") %>%
+  ggbarplot( x = "stage", y = "value",
+             add = c("mean_se", "jitter"),
+             color = "black",
+             fill = "stage",
+             palette = "Blues",
+             width = 0.3,
+             ylim = c(0, 1.3),
+             add.params = list(size = .4),
+             position = position_dodge(0.4)
+             
+  )
 ########################### saving figures
-########################### Clean up
-
-
-
-
-
-
-
-
-
-
+ggsave(siRNA_sang_Fig, file = "siRNA_sang_Fig_2h.pdf", width = 3, height = 4, units = "in", path = "figures/fig_2")
 
 
 
